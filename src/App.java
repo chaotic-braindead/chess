452814dd062f11c5633c60de7c79992a01b7f889
chaotic-braindead <rafaelsebastian0908@gmail.com>
2023-08-20 15:50:15 +0800

    comments

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+// a simple chess app using java. i only started reading jswing documentation literally today and i hate OOP so i hope this works haha
 public class App {
     public static void main(String[] args) throws Exception {
         ChessGUI c = new ChessGUI();
