6418a7358d9fbcd6eaae6abb7e91f44b50ff764d
chaotic-braindead <rafaelsebastian0908@gmail.com>
2023-08-21 13:21:41 +0800

    implemented rook moves

--- src/Rook.java
+++ src/Rook.java
@@ -6,5 +6,50 @@ public class Rook extends Piece {
 
    
     @Override
-    public void setValidMoves(Board b, Square s){}
+    public void setValidMoves(Board b, Square s){
+        this.clearValidMoves();
+        boolean playerColor = this.getIsWhite();
+        boolean oppositeColorHit = false;
+        Square[][] board = b.getBoard();
+
+        // HORIZONTAL
+        // right side
+        for(int i = s.getY() + 1; i < 8; ++i){
+            if(oppositeColorHit) break;
+            Square possibleMove = board[s.getX()][i];
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() == playerColor) break;
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() != playerColor) oppositeColorHit = true;
+            this.addValidMove(possibleMove);
+        }
+        oppositeColorHit = false;
+        // left side
+        for(int i = s.getY() - 1; i >= 0; --i){
+            if(oppositeColorHit) break;
+            Square possibleMove = board[s.getX()][i];
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() == playerColor) break;
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() != playerColor) oppositeColorHit = true;
+            this.addValidMove(possibleMove);
+        }
+        oppositeColorHit = false;
+
+        // VERTICAL
+        // up
+        for(int i = s.getX() - 1; i >= 0; --i){
+            if(oppositeColorHit) break;
+            Square possibleMove = board[i][s.getY()];
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() == playerColor) break;
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() != playerColor) oppositeColorHit = true;
+            this.addValidMove(possibleMove);
+        }
+        oppositeColorHit = false;
+
+        // down
+        for(int i = s.getX() + 1; i <= 7; ++i){
+            if(oppositeColorHit) break;
+            Square possibleMove = board[i][s.getY()];
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() == playerColor) break;
+            if(possibleMove.getPiece() != null && possibleMove.getPiece().getIsWhite() != playerColor) oppositeColorHit = true;
+            this.addValidMove(possibleMove);
+        }
+    }
 }
